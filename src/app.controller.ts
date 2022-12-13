6ba96ccd31f0fcd5402ad1eeb5d8f5a85dfad6cb
Sthiven Melo <sthiven.correia@global.ntt>
2022-12-12 23:19:08 -0300

    :sparkles: Rota Post e Options configuradas

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,35 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Options } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
-
-@Controller()
+import { randomUUID } from 'crypto';
+const OptionsList={
+  'GET':{
+    '/notifications':' Pega todoas as notificações existentes na tabela'
+  },
+  'OPTIONS':{
+    '/notifications': 'Oi bb, vem sempre aqui? Brincadeira. É o que você acabou de usar pra acessar isso'
+  }
+}
+@Controller('notifications')
 export class AppController {
   constructor(private readonly prisma: PrismaService) {}
 
-  @Get('hello')
-  getHello() {
+  @Get()
+  list() {
     return this.prisma.notifications.findMany();
+  }
+  @Post()
+  async create(){
+    await this.prisma.notifications.create({
+      data:{
+        id: randomUUID(),
+        content: 'Você tem uma nova solicitação de amizade!',
+        category: 'Social',
+        recipientId: randomUUID(),
+      }
+    })
+  }
+  @Options()
+  optionsComands(){
+    return OptionsList
   }
 }
