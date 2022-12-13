1222095979dafb38eb037eac50b7c9ab3d27170a
Sthiven Melo <sthiven.correia@global.ntt>
2022-12-13 20:25:13 -0300

    :recycle: Modificando aparência dos testes

--- src/application/entities/notification/content.spec.ts
+++ src/application/entities/notification/content.spec.ts
@@ -1,15 +1,17 @@
 import { Content } from "./content"
+describe("Notification content", () => {
+    it('should be able to create a notification content', () => {
+        const content = new Content('Você recebeu uma solicitação de amizade!')
+    
+        expect(content).toBeTruthy();
+    });
+    
+    it('should be not able to create a notification content with lass than 5 caracters', () => {
+        expect(()=> new Content('BIL!')).toThrow();
+    });
+    
+    it('should be not able to create a notification content with more than 240 caracters', () => {
+        expect(()=> new Content('A'.repeat(241))).toThrow();
+    });
+})
 
-test('it should be able to create a notification content', () => {
-    const content = new Content('Você recebeu uma solicitação de amizade!')
-
-    expect(content).toBeTruthy();
-});
-
-test('it should be not able to create a notification content with lass than 5 caracters', () => {
-    expect(()=> new Content('BIL!')).toThrow();
-});
-
-test('it should be not able to create a notification content with more than 240 caracters', () => {
-    expect(()=> new Content('A'.repeat(241))).toThrow();
-});
