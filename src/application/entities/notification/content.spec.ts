437a77e54417fb314e59bc28804c291e5f3e21b5
Sthiven Melo <sthiven.correia@global.ntt>
2022-12-13 20:22:54 -0300

    :sparkles: Primeiros testes

--- src/application/entities/notification/content.spec.ts
+++ src/application/entities/notification/content.spec.ts
@@ -4,4 +4,12 @@ test('it should be able to create a notification content', () => {
     const content = new Content('Você recebeu uma solicitação de amizade!')
 
     expect(content).toBeTruthy();
-})
+});
+
+test('it should be not able to create a notification content with lass than 5 caracters', () => {
+    expect(()=> new Content('BIL!')).toThrow();
+});
+
+test('it should be not able to create a notification content with more than 240 caracters', () => {
+    expect(()=> new Content('A'.repeat(241))).toThrow();
+});
